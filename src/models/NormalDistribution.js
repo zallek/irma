45b19db00a98f5404514f2b39b63ac6e4b508476
Nicolas Fortin <nicolas.fortin37@gmail.com>
2015-12-10 23:31:10 +0100

    Temp commit

---
+++ src/models/NormalDistribution.js
@@ -0,0 +1,18 @@
+import normalDistribution from 'distributions-normal';
+
+
+class LinearModel {
+  constructor(releases) {
+    this.releases = releases;
+  }
+
+  probalityReleaseInDays(nbDays) {
+
+  }
+
+  probalityReleaseWithInDays(nbDays) {
+    
+  }
+}
+
+export default LinearModel;
---
+++ src/utils/math.js
@@ -0,0 +1,4 @@
+
+export function mean(a, b) {
+  return (a - b) / 2;
+}
